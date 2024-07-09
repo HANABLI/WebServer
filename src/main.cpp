abf866ac21cf41c425b3e9ac1c8759ea68d5859d
Hatem Nabli <hatem.nabli@actemium.com>
2024-07-09 23:05:53 +0200

    feat: memory leak detection when running Debug build on windows

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,15 @@
  * This module holds the main() function, which is the 
  * entrypoint to the webServer program.s
  */
-
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 #include <signal.h>
 #include <thread>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string>
 #include <chrono>
-
+#include <SystemUtils/DiagnosticsStreamReporter.hpp>
 #include <Http/Server.hpp>
 #include <HttpNetworkTransport/HttpServerNetworkTransport.hpp>
 
@@ -49,11 +50,19 @@ void InterruptHandler(int sig) {
  *      This is the array of command-line arguments given to the program.
  */
 int main(int argc, char* argv[]) {
+#ifdef _WIN32
+    _crtBreakAlloc = 1708;
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif /* _WIN32 */
     auto transport = std::make_shared< HttpNetworkTransport::HttpServerNetworkTransport >();
     Http::Server server;
+        const auto diagnosticsSubscription = server.SubscribeToDiagnostics(
+        SystemUtils::DiagnosticsStreamReporter(stdout, stderr)
+    );
     if (!server.Mobilize(transport, 8080)) {
         return EXIT_FAILURE;
     }
+    auto testLeak = new char[80];
     const auto previousInterruptHandler = signal(SIGINT, InterruptHandler);
     printf("Web server starting up.\n");
     while (!shutDown) {
