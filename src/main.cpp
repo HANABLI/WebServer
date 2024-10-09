bebfd89467a54065b021cf1a839bf07f7d9be258
HNABLI <nablihatem3@gmail.com>
2024-10-09 15:27:26 +0200

    feat: looking for the configuration file in differents paths
    * look in path provided by user if -c or --config command-line args are given.
    * look for configuration file in workin directory if no configuration args
    or if no file found in the user-provided path

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,72 @@ namespace {
      * should shut down.
      */
     bool shutDown = false;
+
+    /**
+     * This contains variables set throuth the operating system environment
+     * or the command-line arguments
+     */
+    struct Environment {
+        /**
+         * This is the path to the configuration file to use to configure
+         * the server.
+         */
+        std::string configFilePath;
+    };
+}
+
+
+/**
+ * This function updates the program environement to incorporate
+ * an applicable command-line arguments.
+ * 
+ * @param[in] argc
+ *      This is the number of command-line arguments to the program.
+ * 
+ * @param[in] argv
+ *      This is the array of command line arguments given to the program.
+ * 
+ * @param[in] environement
+ *      This is the environement to update.
+ */
+bool ProcessCommandLineArguments(
+    int argc,
+    char* argv[],
+    Environment& environment
+) {
+    size_t state = 0;
+    for( int i = 1; i < argc; ++i) {
+        const std::string arg(argv[i]);
+        switch (state)
+        {
+            case 0: { // next argument
+                if ((arg == "-c") || (arg == "--config")) {
+                    state = 1;
+                } else {
+                    fprintf(stderr, "error: unrecognized option: '%s'\n", arg.c_str());
+                    return false;
+                }
+            } break;
+            
+            case 1: { // -c| --config
+                if (!environment.configFilePath.empty()) {
+                    fprintf(stderr, "error: multiple configuration file paths given\n");
+                    return false;
+                }
+                environment.configFilePath = arg;
+                state = 0;
+            } break;
+            default:
+                break;
+        }
+    }
+    switch (state)
+    {
+        case 1: {
+            fprintf(stderr, "error: configuration file path expected\n");
+        } return false;
+    }
+    return true;
 }
 
 /**
@@ -50,32 +116,53 @@ void InterruptHandler(int sig) {
  * This function opens and reads the server's configuration file,
  * returning it. The configuration is formatted as a JSON object.
  * 
+ * @param[in] environment
+ *      This contains variables set through the operating system
+ *      environment or the command-line arguments.
+ * 
  * @return
  *      The server's configuration is returned as a JSON obejct.
  */
-Json::Json ReadConfiguration() {
+Json::Json ReadConfiguration(const Environment& environment) {
     // Default configuration to be used when there are any issues
     // reading the actual configuration file.
     Json::Json configuration(Json::Json::Type::Object);
     configuration.Set("port", DEFAULT_PORT);
+
     // Open the configuration file.
-    const auto configFile = std::shared_ptr< FILE >( fopen(
-            (SystemUtils::File::GetExeParentDirectory() + "/config.json").c_str(),
-            "rb"
-        ),
-        [](FILE* f){
-            if (f != NULL) {
-                (void)fclose(f);
+    std::vector< std::string > possibleConfigPaths = {
+        "config.json",
+        SystemUtils::File::GetExeParentDirectory() + "/config.json",
+
+    };
+    if (!environment.configFilePath.empty()) {
+        possibleConfigPaths.insert(
+            possibleConfigPaths.begin(),
+            environment.configFilePath
+        );
+    }
+    std::shared_ptr< FILE > configFile;
+    for (const auto& possibleConfigPath: possibleConfigPaths ) {
+        configFile = std::shared_ptr< FILE >( 
+            fopen(possibleConfigPath.c_str(), "rb"),
+            [](FILE* f){
+                if (f != NULL) {
+                    (void)fclose(f);
+                }
             }
+        );
+        if (configFile != NULL) {
+            break;
         }
-    );
+    }
 
     if (configFile == NULL) {
+        fprintf(stderr, "error: unable to open the configuration file\n");
         return configuration;
     } 
     // Determine the size of the configuration file.
     if(fseek(configFile.get(), 0, SEEK_END) != 0) {
-        fprintf(stderr, "error: unable to  seek to the ned of the configuration file\n");
+        fprintf(stderr, "error: unable to  seek to the end of the configuration file\n");
         return configuration;
     }
     const auto configSize = ftell(configFile.get());
@@ -112,14 +199,20 @@ Json::Json ReadConfiguration() {
  * @param[in] transport
  *      This is the transport layer to give to the server for interfacing
  *      with the network.
+ * 
+ * @param[in] environment
+ *      This contains variables set through the operating system
+ *      environment or the command-line arguments.
+ * 
  * @return
  *      An indication of whether or not the function succeeded is returned.
  */
 bool ConfigureAndStartServer(
     Http::Server& server,
-    std::shared_ptr< Http::ServerTransportLayer > transport
+    std::shared_ptr< Http::ServerTransportLayer > transport,
+    const Environment& environment
 ) {
-    const auto configuration = ReadConfiguration();
+    const auto configuration = ReadConfiguration(environment);
     uint16_t port = 0;
     if (configuration.Has("port")) {
         port = (int)*configuration["port"];
@@ -151,11 +244,15 @@ int main(int argc, char* argv[]) {
 #endif /* _WIN32 */
     auto transport = std::make_shared< HttpNetworkTransport::HttpServerNetworkTransport >();
     const auto previousInterruptHandler = signal(SIGINT, InterruptHandler);
+    Environment environment;
+    if (!ProcessCommandLineArguments(argc, argv, environment)) {
+        return EXIT_FAILURE;
+    }
     Http::Server server;
         const auto diagnosticsSubscription = server.SubscribeToDiagnostics(
         SystemUtils::DiagnosticsStreamReporter(stdout, stderr)
     );
-    if (!ConfigureAndStartServer(server, transport)) {
+    if (!ConfigureAndStartServer(server, transport, environment)) {
         return EXIT_FAILURE;
     }
     auto testLeak = new char[80];
