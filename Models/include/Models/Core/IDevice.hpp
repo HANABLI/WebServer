4eb5e5d03c850041cc849e7c625fb734cbc0a5d9
HNABLI <nablihatem3@gmail.com>
2026-01-07 13:38:39 +0100

    feat: declaration of the IDevice interface that descrabe the capabilitie of CRTP Device

---
+++ Models/include/Models/Core/IDevice.hpp
@@ -0,0 +1,35 @@
+#ifndef MQTT_PLUGIN_DEVICE_HPP
+#define MQTT_PLUGIN_DEVICE_HPP
+
+// IDevice.hpp
+#pragma once
+
+#include <string>
+#include <Json/Json.hpp>
+
+namespace FalcataIoTServer
+{
+    class IDevice
+    {
+    public:
+        virtual ~IDevice() = default;
+
+        // Identité / métadonnées
+        virtual const std::string GetId() const = 0;
+        virtual void SetId(const std::string& id) = 0;
+
+        virtual const std::string GetName() const = 0;
+        virtual void SetName(const std::string& name) = 0;
+
+        // "kind" = catégorie fonctionnelle (server, sensor, gateway, etc.)
+        virtual const std::string GetKind() const = 0;
+
+        // "protocol" = mqtt, opcua, modbus-tcp…
+        virtual const std::string GetProtocol() const = 0;
+
+        // Actif / inactif
+        virtual bool IsEnabled() const = 0;
+        virtual void SetEnabled(bool enabled) = 0;
+    };
+}  // namespace FalcataIoTServer
+#endif
