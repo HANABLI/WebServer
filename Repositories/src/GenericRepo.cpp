781f495f50cacdf04c0dfa9b19602658a16a655c
HNABLI <nablihatem3@gmail.com>
2026-01-27 00:00:35 +0100

    fix: create .cpp file for GenericRepository tocentralize template compilation.

---
+++ Repositories/src/GenericRepo.cpp
@@ -0,0 +1,5 @@
+/**
+ * This translation unit exists mainly to give the Repositories library
+ * at least one .cpp file and to centralize template compilation.
+ */
+#include <Repositories/GenericRepo.hpp>
